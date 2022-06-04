6fefe2a69c16f8bd28ae8d7d55d9c7fa9980674b
Charles Bilbo <cbilbo9@gmail.com>
2022-06-04 01:57:31 -0500

    add basic multiplayer

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,43 +1,91 @@
 <template>
-<div class="flex h-screen">
-  <div class="m-auto">
-    <div>
-      <div data-player-position="top" class="grid grid-cols-5 justify-items-center">
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
+  <div>
+    <div class="flex h-screen">
+      <div class="m-auto" v-if="!submitted">
+        <div class="w-full max-w-xs text-center">
+          <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+              <input v-model="currentPlayer" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+            </div>
+            <div class="flex items-center justify-center">
+              <button @click="addPlayer(); sendNewPlayerSocket()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                Enter
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div class="card-table m-5 flex">
-        <div class="m-auto reveal align-middle font-extrabold"> Reveal Cards</div>
-      </div>
-      <div data-player-position="bottom" class="h-28 grid grid-cols-5 justify-items-center">
-          <div class="card"></div>
-          <div class="card"></div>
-          <div class="card"></div>
-          <div class="card"></div>
-          <div class="card"></div>
+      <div class="m-auto" v-if="submitted">
+        <div>
+          <div ref="top" data-player-position="top" :class="{'grid-cols-': grid.top}" class="grid justify-items-center">
+            <div v-for="card in cards.top" :key="card.name" :data-current-player="card.main">
+              <div class="card mx-auto"></div>
+              <div>
+                <span class="text-white font-bold mt-1">
+                  {{card.name}}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card-table m-5 flex">
+            <div class="m-auto reveal align-middle font-extrabold"> Reveal Cards</div>
+          </div>
+          <div ref="bottom" data-player-position="bottom" :class="{'grid-cols-': grid.top}" class="h-28 grid justify-items-center">
+            <div v-for="card in cards.bottom" :key="card.name" :data-current-player="card.main">
+              <div class="card mx-auto"></div>
+              <div>
+                <span class="text-white font-bold mt-1">
+                  {{card.name}}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 import { io } from 'socket.io-client';
+import _ from 'lodash';
+
 export default {
   data:() => ({
-    socket: io('http://localhost:8080')
+    socket: io('http://localhost:8080'),
+    players: [],
+    currentPlayer: null,
+    submitted: false,
+    cards: {
+      top: [],
+      bottom: []
+    },
+    grid: {
+      top: 0,
+      bottom: 0
+    }
   }),
   methods: {
-    sendWeb() {
-      this.socket.emit("user-connected");
+    addPlayer(main = true, name = this.currentPlayer) { 
+      this.submitted = true
+      this.cards.top.length > this.cards.bottom.length 
+        ? this.cards.bottom.push({'name': name, 'main': true, 'score': null})
+        : this.cards.bottom.push({'name': name, 'main': true, 'score': null})
+    },
+    sendNewPlayerSocket()
+    {
+      this.socket.emit('new-player-connected', {
+        'name': this.currentPlayer
+      })
     }
-  },
-  mounted() { 
-    this.$nextTick(() => {
-      this.socket.on('accepted', () => console.log(true))
+  }, 
+  mounted() {
+    this.$nextTick(() => { 
+      this.socket.on('new-player', (data) => {
+        if(data.name != this.currentPlayer) { 
+          this.addPlayer(false, data.name)
+        }
+      })
     })
   }
 }
@@ -62,5 +110,11 @@ export default {
    padding: 5px;
    border-radius: .5rem;
    font-size: x-large;
+ }
+ .user-input { 
+   background: white;
+   height:10rem;
+   width: 35rem;
+   border-radius: 1rem;
  }
 </style>
